23bd54d84dcdc609acea0477cc38422be30388e6
Prasoon Pandey <prasoonpandey06@gmail.com>
2021-05-21 01:51:45 +0530

    Take input from the user

--- app/src/main/java/com/prasoon/randomnumbergenerator/MainActivity.java
+++ app/src/main/java/com/prasoon/randomnumbergenerator/MainActivity.java
@@ -2,9 +2,21 @@ package com.prasoon.randomnumbergenerator;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Context;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.EditText;
+import android.widget.Toast;
 
 public class MainActivity extends AppCompatActivity {
+
+    public void generateNum(View view){
+        EditText minTextView = (EditText) findViewById(R.id.minValInput);
+        EditText maxTextView = (EditText) findViewById(R.id.maxValInput);
+
+        int min = Integer.parseInt(minTextView.getText().toString());
+        int max = Integer.parseInt(maxTextView.getText().toString());
+    }
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
