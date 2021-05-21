59bd013eff638738e127c2b859bb3a83c9d426f5
Prasoon Pandey <prasoonpandey06@gmail.com>
2021-05-21 11:16:06 +0530

    Improve ui design

--- app/src/main/java/com/prasoon/randomnumbergenerator/MainActivity.java
+++ app/src/main/java/com/prasoon/randomnumbergenerator/MainActivity.java
@@ -4,6 +4,8 @@ import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
 import android.view.View;
+import android.view.Window;
+import android.view.WindowManager;
 import android.widget.EditText;
 import android.widget.TextView;
 import android.widget.Toast;
@@ -37,6 +39,10 @@ public class MainActivity extends AppCompatActivity {
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+        requestWindowFeature(Window.FEATURE_NO_TITLE); //will hide the title
+        getSupportActionBar().hide(); // hide the title bar
+        this.getWindow().setFlags(WindowManager.LayoutParams.FLAG_FULLSCREEN,
+                WindowManager.LayoutParams.FLAG_FULLSCREEN); //enable full screen
         setContentView(R.layout.activity_main);
 
         minTextView = findViewById(R.id.minValInput);
